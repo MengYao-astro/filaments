6659ade8a7350c050d246376f111388986560b50
Yao Meng <yao@student1.strw.leidenuniv.nl>
2018-10-10 21:34:13 +0200

    renamed:    diffRflat2D1.fits -> FLMTexample.fits 	renamed:    test -> header.py

--- header.py
+++ header.py
@@ -9,10 +9,12 @@ Created on Sun Oct  7 19:00:43 2018
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
-'''
+
 fits_image_filename = fits.util.get_testdata_filepath('test0.fits')
 hdul = fits.open(fits_image_filename)
-hdul[0].header['DATE']
-'''
-f=np.arange(1,5,1)
-print(f)
+#hdul.info()
+#print(hdul[0].header['DATE'])
+hdr = hdul[0].header
+#print(list(hdr.keys()))
+hdr.set('observer', 'Meng Yao')
+print(hdr['observer'])
