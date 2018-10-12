0565c0a05ce513124c9c033ee6910a3f220ac569
Yao Meng <yao@student1.strw.leidenuniv.nl>
2018-10-12 17:43:40 +0200

    new file:   2timeD.png 	modified:   2timesD.fits 	modified:   diffDFLMTs.py

--- diffDFLMTs.py
+++ diffDFLMTs.py
@@ -16,17 +16,17 @@ def coldsty(r,Ap,rho,Rflat,p):
 #create a typical filament
 #r range
 
-r=np.arange(-50,51,2)
+r=np.linspace(-100,100,101)
 Ap=np.pi/2.
 rho=2.
 Rflat=10.
 p=2.
-tFLMT=np.zeros((51,51))
-for i in range(51):
+tFLMT=np.zeros((101,101))
+for i in range(101):
  tFLMT[i,:]=coldsty(r,Ap,rho,Rflat,p)
 plt.plot(r,tFLMT[0,:])
 plt.show()
 #write to fits
 hdu=fits.PrimaryHDU(tFLMT)
 hdul=fits.HDUList([hdu])
-hdul.writeto('tFLMT.fits')
+hdul.writeto('2timesD.fits')
