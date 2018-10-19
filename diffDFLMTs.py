0ddc28efa6112a9cfb9769e8b655dd5497704579
Yao Meng <yao@student1.strw.leidenuniv.nl>
2018-10-19 17:29:58 +0200

    modified:   diffDFLMTs.py

--- diffDFLMTs.py
+++ diffDFLMTs.py
@@ -16,8 +16,8 @@ def coldsty(r,Ap,rho,Rflat,p):
 #create filament in different distance
 d=float(input('distance in pc:'))
 #r range
-times=d/100.
-r=np.linspace(-5,5,101)*times
+times=d/460.
+r=np.linspace(-3,3,101)*times
 Ap=np.pi/2.
 rho=1.8
 Rflat=0.05
@@ -28,8 +28,8 @@ for i in range(101):
 plt.plot(r,tFLMT[0,:])
 plt.show()
 #write to fits
-'''
+
 hdu=fits.PrimaryHDU(tFLMT)
 hdul=fits.HDUList([hdu])
 hdul.writeto('FLMTin%dpc.fits' %d)
-'''
+
