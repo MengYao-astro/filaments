a9b6e549d85a066e988276a00f9d9ddb1f121d8e
Yao Meng <yao@student1.strw.leidenuniv.nl>
2018-10-24 15:19:54 +0200

    modified:   diffDFLMTs.py

--- diffDFLMTs.py
+++ diffDFLMTs.py
@@ -6,7 +6,7 @@ Created on 11 oct 2018 10:00
 @author: meng
 """
 
-
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
@@ -17,19 +17,21 @@ number=sample[:,0]
 rho=sample[:,1]
 p=sample[:,2]
 Rflat=sample[:,3]
+#count
+amount=len(number)
 
 #Filament function
 def coldsty(r,Ap,rho,Rflat,p):
     return Ap*(rho*Rflat)*1./np.power(1.+(r/Rflat)*(r/Rflat),(p-1.)/2.)
 #create filament in different distance
-#distance=float(input('distance in pc:'))
-distance=460.
+distance=float(input('distance in pc:'))
+initialD=460.
 #r range
-times=distance/460.
+times=distance/initialD
 r=np.linspace(-1.5,1.5,1345)*times
 Ap=np.pi/2.
-diffdF=np.zeros((17,1345,1345))
-for i in range(17):
+diffdF=np.zeros((amount,1345,1345))
+for i in range(amount):
  for j in range(1345):
   diffdF[i,j,:]=coldsty(r,Ap,rho[i],Rflat[i],p[i])
  plt.plot(r,diffdF[i,0,:])
@@ -37,6 +39,8 @@ for i in range(17):
 
  hdu=fits.PrimaryHDU(diffdF[i,:,:])
  hdul=fits.HDUList([hdu])
- hdul.writeto('No%dFLMTin%dpc.fits' %(number[i],distance))
+ if not os.path.exists('FLMTSatdiffd/No%d' %number[i]):
+  os.mkdir('FLMTSatdiffd/No%d' %number[i])
+ hdul.writeto('FLMTSatdiffd/No%d/No%dFLMTat%dpc.fits' %(number[i],number[i],distance))
 
 plt.show()
