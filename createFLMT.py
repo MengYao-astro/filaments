a981ff05ee8f66533b0c9af64601c8ebe390ccad
Yao Meng <yao@tulor.strw.leidenuniv.nl>
2019-02-20 14:19:03 +0100

    modified:   createFLMT.py

--- createFLMT.py
+++ createFLMT.py
@@ -34,8 +34,9 @@ def coldsty(vr,vAp,vrhoc,vRflat,vp):                #unit: cm-2
 #define pixel size and calculate the physical size of the filaments
 #1'' per pixel and take 1024 pixels
 datapoints=1024
+pixelsize=1.   # unit : arcsec
 #set distance to 500pc
-physicalLpc=1024/206265.*500. # unit : pc
+physicalLpc=datapoints*pixelsize/206265.*500. # unit : pc
 physicalLcm=physicalLpc*3.0856775814914*10.**18. # unit : cm
 #r range
 r=np.linspace(-physicalLcm,physicalLcm,datapoints)   # unit: cm
@@ -48,7 +49,7 @@ rhoc=2.33*(1.6737236*10.**-24.)*NH2c/Ap/Rflat     # unit: cm-3
 NH2=np.zeros((amount,datapoints))
 for i in range(amount):
  NH2[i,:]=coldsty(r,Ap[i],rhoc[i],Rflat[i],p[i])/(2.33*1.6737236*10.**-24.)
- plt.plot(r,NH2[i,:])
+ #plt.plot(r,NH2[i,:])
  #write to fits file
  '''
  hdu=fits.PrimaryHDU(NH2[i,:])
@@ -56,22 +57,25 @@ for i in range(amount):
  if not os.path.exists('Cdensityatdiffd/No%d' %number[i]):
   os.mkdir('Cdensityatdiffd/No%d' %number[i])
  hdul.writeto('Cdensityatdiffd/No%d/No%dcoldensityat%dpc.fits' %(number[i],number[i],distance))
- '''
-plt.title('column density (cm^-2)')
-plt.show()
+'''
+#plt.title('column density (cm^-2)')
+#plt.show()
 
-#Flux of the filaments
-def flux(vNH2coldsty,vTem,vwavelen,vopacity,vthetaH):
-    return vNH2coldsty/(2.02*10.**20)/(np.exp(1.439/vwavelen/(vTem/10.))-1)/vwavelen**3*(vopacity/0.01)*(vthetaH/10.)**2
+#Flux per beam of the filaments
+def fluxperbeam(vNH2,vT,vlambda,vkv,vOmega_A):
+    return 4.369*10**-23*vNH2/(np.exp(1.439/vlambda/(vT/10.))-1)*vlambda**-3*(vkv/0.01)*vOmega_A
     #NH2 in cm-2, Tem in K, wavenlen in mm, opacity in cm2/g, thetaH in arcsec
     #get flux in mJy/beam
+#Flux per pixel
+def fluxperpixel(vNH2,vT,vlambda,vkv,vtheta):
+    return 3.9167*10**-25*vNH2/(np.exp(1.439/vlambda/(vT/10.))-1)*vlambda**-3*vkv*vtheta**2
+    #NH2 in cm-2, Tem in K, lambda in mm, kv in cm2/g, theta in arcsec
+    #get flux in Jy/beam
 #set parameters
 Tem=10.        # in K
 wavelen=1.     # in mm
 opacity=0.01     # in cm2/g
-thetaH=16.      # in arcsec
-Fflux=flux(NH2,Tem,wavelen,opacity,thetaH)
-Fflux=Fflux/1000.                                  # convert unit from mJy to Jy
+Fflux=fluxperpixel(NH2,Tem,wavelen,opacity,pixelsize)
 plt.plot(r,Fflux[0,:])
 plt.title('flux density (mJy/beam)')
 plt.show()
